4ba6b20e758fe6ac225af1ca46f8f0c8bb60158b
Julien Schueller <schueller@phimeca.com>
2017-05-10 09:08:35 +0200

    Update to 1.9

---
+++ upload_sf.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+user=$1
+
+release=1.9
+for _basename in otpmml-1.4 otsvm-0.3 otrobopt-0.2 otmixmod-0.5 otmorris-0.2 otlm-0.4 otfftw-0.4
+do
+  project=`echo "${_basename}" | cut -d '-' -f 1`
+  version=`echo "${_basename}" | cut -d '-' -f 2`
+
+  # source
+  wget -c https://github.com/openturns/${project}/archive/v${version}.tar.gz -O /tmp/${_basename}.tar.gz
+
+  for pybasever in 2.7 3.5 3.6
+  do
+    for _arch in i686 x86_64
+    do
+      _file=${_basename}-py${pybasever}-${_arch}.exe
+      wget -c https://github.com/openturns/build-modules/releases/download/v${release}/${_file} -P /tmp
+    done
+  done
+done
+
+sha256sum /tmp/*.tar.gz
+sha256sum /tmp/*.exe
